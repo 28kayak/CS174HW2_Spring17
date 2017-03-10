557af9149c9b1ec8f8a76cf82eb94885919c78fe
28kayak <kaya.ota@sjsu.edu>
2017-03-10 14:34:54 -0800

    edit

--- parser.js
+++ parser.js
@@ -1,4 +1,21 @@
-function parse(){
-  
+function loadXMLDoc(url){
+  if(window.XMLHttpRequest)
+  {
+    //for IE7+, Firefox,Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  }
+  else
+  {
+    //for IE5, IE6
+    xmlhttp = nwe ActiveXObject();
+    //ActiveXObject will be loaded only for IEs
+  }
+  if(this.readyState == 4 && this.status == 200)
+  {
+    format2Table(this);
+  }
+  xmlhttp.open("GET", url, false);
+  xmlhttp.send();
+
 
 }
